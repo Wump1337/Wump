6280989732793d3b2ab44af7045b26bb218cfec1
Wump <nicoslafter0@protonmail.com>
2021-07-14 09:59:21 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+document.write(`<h1> Welcome to My Site ! :)</h1>
+</br>
+<h2>Hey, there! Its me Wump a growing YouTuber with 64 Subscribers a dream of hitting 1,000 subscribers</h2>
+<center>
+<a href="https://www.youtube.com/channel/UCC1TW4kMMdmrfWXBX8nRnWg">
+    <img src="./assets/favicon.png" alt="Wump" height="100px" width="100px">
+</a>
+<br>
+<div>
+    <h4>
+        Made with <img src="./assets/love.svg" alt="" height="30px" width="30px"> by Wump.
+    </h4>
+</div>
+
+
+<section class="projects">
+    <br>
+    <h1>My Current Projects</h1>
+    <hr>
+    <section>
+        <h2>
+            <a class="link1" href="https://wiseengine.netlify.app/">
+                WiseEngine
+            </a> - An HTML5 Game Engine for making web games.
+        </h2>
+    </section>
+    <hr>
+    <section>
+    <h2>
+        <a class="link1" href="https://xoniaapp.com">
+            Xonia
+        </a>
+        
+        I don't contribute anymore but still support Xonia a lot! Please consider trying Xonia, if you don't like it, let developers@quacci.xyz know!
+
+    </h2>
+</section>
+    <hr>
+    <br>
+</section>
+
+
+
+<section class="links">
+    <a class="twitter1" href="https://twitter.com/TheWumpp">Twitter</a>
+    <a class="youtube1" href="https://www.youtube.com/channel/UCC1TW4kMMdmrfWXBX8nRnWg">YouTube</a>
+    <a class="github1" href="https://github.com/Wump1337">GitHub</a>
+</section>
+
+</br>
+
+<div class="cp">
+    &copy; 2021 Wump
+</div>
+
+</center>
+`)
