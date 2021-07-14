00028615f5655e7fa0a2d784bb5859fb4bd90e6c
Wump <nicoslafter0@protonmail.com>
2021-07-14 10:03:44 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,11 +6,6 @@ document.write(`<h1> Welcome to My Site ! :)</h1>
     <img src="./assets/favicon.png" alt="Wump" height="100px" width="100px">
 </a>
 <br>
-<div>
-    <h4>
-        Made with <img src="./assets/love.svg" alt="" height="30px" width="30px"> by Wump.
-    </h4>
-</div>
 
 
 <section class="projects">
@@ -41,16 +36,19 @@ document.write(`<h1> Welcome to My Site ! :)</h1>
 
 
 
-<section class="links">
-    <a class="twitter1" href="https://twitter.com/TheWumpp">Twitter</a>
-    <a class="youtube1" href="https://www.youtube.com/channel/UCC1TW4kMMdmrfWXBX8nRnWg">YouTube</a>
-    <a class="github1" href="https://github.com/Wump1337">GitHub</a>
-</section>
-
 </br>
 
 <div class="cp">
     &copy; 2021 Wump
+    <div>
+    <section class="links">
+    <a class="twitter1" href="https://twitter.com/TheWumpp">Twitter</a>
+    <a class="youtube1" href="https://www.youtube.com/channel/UCC1TW4kMMdmrfWXBX8nRnWg">YouTube</a>
+    <a class="github1" href="https://github.com/Wump1337">GitHub</a>
+    <h4>
+        Made with <img src="./assets/love.svg" alt="" height="30px" width="30px"> by Wump.
+    </h4>
+</div>
 </div>
 
 </center>
